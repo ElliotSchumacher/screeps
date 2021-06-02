77bc1229a11baa2d26637d8913c0a35b9423792f
Elliot Schumacher <Elliot.l.schumacher.code@gmail.com>
2021-06-02 02:11:37 -0700

    removed loop from upgrader role

--- templates/role.upgrader.js
+++ templates/role.upgrader.js
@@ -1,18 +1,4 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-
-module.exports.loop = function () {
-
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-    }
-}var roleUpgrader = {
+var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
