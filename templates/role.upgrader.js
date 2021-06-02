c6e5280837b16b77fa97cda64bfd71f898002581
Elliot Schumacher <Elliot.l.schumacher.code@gmail.com>
2021-06-02 01:58:34 -0700

    Templates from tutorial

---
+++ templates/role.upgrader.js
@@ -0,0 +1,43 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+    }
+}var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
+	    }
+	    if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.upgrading = true;
+	        creep.say('⚡ upgrade');
+	    }
+
+	    if(creep.memory.upgrading) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+	}
+};
+
+module.exports = roleUpgrader;
