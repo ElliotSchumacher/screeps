ed149365875ade9ca29edaff00f5e468e8254a6a
Elliot Schumacher <elliot.l.schumacher.code@gmail.com>
2023-03-12 12:05:23 -0700

    Link: Removed repetitive calls to get link object

--- structure.link.js
+++ structure.link.js
@@ -2,12 +2,17 @@ var structureLink = {
 
     /** @param {Link} link **/
     run: function(link) {
-        let isSenderLink = Game.getObjectById(link.room.memory.links[link.id]) != undefined;
+        let linkData = link.room.memory.links[link.id];
+        let isSenderLink = linkData != undefined;
+        // console.log("linkId: " + link.id);
+        // console.log("linkData: " + linkData);
+        // console.log("isSenderLink: " + isSenderLink);
         if (isSenderLink) {
-            let targetLink = Game.getObjectById(link.room.memory.links[link.id].targetLink);
+            let targetLink = Game.getObjectById(linkData.targetLink);
             let currentStore = link.store.getUsedCapacity(RESOURCE_ENERGY);
             if (currentStore > 0) {
-                link.transferEnergy(targetLink);
+                // console.log(link.transferEnergy(targetLink));
+                link.transferEnergy(targetLink)
             }
         }
 	}
